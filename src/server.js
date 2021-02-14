e831bd542baad1702b3edc59ff7a138f6cd48459
ZackTorretta <ztorretta@ltu.edu>
2021-02-13 20:25:29 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const app = express();
 
+const id = 'Hello World';
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send(id);
+  // res.send('Hello World');
 });
 
 app.listen(8080);
